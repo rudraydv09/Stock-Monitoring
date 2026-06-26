7f02420ecba72c2f551d1f3ce59a96455db4453a
Rudra Pratap Singh Yadav <rudra.yad4412@gmail.com>
2026-06-26 16:38:28 +0530

    Updated frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
 import Signup from './landing_page/signup/Signup';
