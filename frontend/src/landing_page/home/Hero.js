3da6479345eeeca6d261ad0168c74aeb8660089e
Rudra Pratap Singh Yadav <rudra.yad4412@gmail.com>
2026-06-26 16:51:43 +0530

    Updated frontend

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
     return (
@@ -20,7 +21,7 @@ function Hero() {
                         ETFs, bonds, and more.
                     </p>
 
-                    <button
+                    <Link to='https://stock-monitoring-dashboard.onrender.com'
                         className="btn btn-primary btn-lg mt-3"
                         style={{
                             width: "250px",
@@ -28,7 +29,7 @@ function Hero() {
                         }}
                     >
                         Dashboard
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
